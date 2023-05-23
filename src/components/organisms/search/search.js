ef110940cd383538712636109ea7a1b68e41d954
Sachin2903 <sachin.18bme1170@abes.ac.in>
2023-05-23 07:26:24 +0530

    auto_fill_google_search_bar

--- src/components/organisms/search/search.js
+++ src/components/organisms/search/search.js
@@ -3,19 +3,29 @@ import styles from "../../../Styles/searchStyle/search.module.css"
 import {AiOutlineSearch} from 'react-icons/ai';
 import {MdKeyboardVoice} from 'react-icons/md';
 export function Search(){
+    const autoinput=useRef("");
     const [hints,setHints]=useState([]);
 
     function selecthintsfun(event){
-      
-       
-       fetch("http://cdn-api.co-vin.in/api/v2/admin/location/states")
+        if(event.target.value.length===0){
+            setHints([])
+            return;    
+    }
+    
+
+        fetch("http://cdn-api.co-vin.in/api/v2/admin/location/states")
        .then((res)=>res.json())
        .then((data)=>data.states)
        .then((detail)=>{
-        setHints(detail.filter((e)=>e.state_name.includes(event.target.value)))
+        setHints(detail.filter((e)=>e.state_name.toUpperCase().includes(event.target.value.toUpperCase())))
        }).catch(er =>alert("please wait"))
         
 
+    }
+
+    function autoFill(id){
+        autoinput.current.value=hints[id].state_name;
+        setHints([]);
     }
 
     return(
@@ -24,11 +34,11 @@ export function Search(){
              <div className={styles.boxaline}>
                 <div className={styles.searchbox}>
                     <AiOutlineSearch className={styles.searchicon}/>
-                     <input onChange={selecthintsfun} className={styles.search} type="text" placeholder="Search"/>
+                     <input ref={autoinput} onChange={selecthintsfun} className={styles.search} type="text" placeholder="Search"/>
                      <MdKeyboardVoice className={styles.searchicon}/>
                 </div>
                 <div className={styles.searchhint}>{
-                    hints.map((e,i)=> <p key={i*10}>{e.state_name}</p>)
+                    hints.map((e,i)=> <p className={styles.hinttext}  key={i*10} onClick={()=>autoFill(i)}>{e.state_name}</p>)
                   
                  
                 }</div>
