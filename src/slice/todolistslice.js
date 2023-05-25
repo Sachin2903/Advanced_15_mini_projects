7c23a432d45a744f3f972af5578bef5420d7dad0
Sachin2903 <sachin.18bme1170@abes.ac.in>
2023-05-25 08:49:05 +0530

    done

--- src/slice/todolistslice.js
+++ src/slice/todolistslice.js
@@ -1,32 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-export const todolistSlice=createSlice({
-    initialState:[],
-    name:"todo",
-    reducers:{
-        savedetails:(state,action)=>{
-            let i={
-               Name:action.payload,
-               id:state.length,
-               status:"pending"
-            }
-           return [...state,i];
+export const todolistSlice = createSlice({
+    initialState: [],
+    name: "todo",
+    reducers: {
+        savedetails: (state, action) => [...state, { Name: action.payload, id: state.length, status: "pending" }],
 
-        },
-        changemystatus:(state,action)=>{
-           
-         state.map((e,i)=>{
-           
-             if(i==action.payload){
-                
-                (e.status==="pending")?e.status="complete": e.status="pending"
-            
-            
 
-             }
-            
-        })
-        return state;
+        changemystatus: (state, action) => {
+        //    let a=state.map((e) => e);
+        //     console.log(state);
+
+
+
+            state.map((e, i) => {
+
+                if (i == action.payload) {
+                    (e.status === "pending") ? e.status = "complete" : e.status = "pending"
+                }
+
+            })
         }
 
     }
